a633b7cdbb3d238bae3972cd3370da9d88184d70
Claire Gregg <clairegregg@clairegregg.com>
2024-11-06 11:40:02 +0000

    Make whales actually use Bob2
    Current version appears to have checksum failing around 50% of the time...

--- whales.py
+++ whales.py
@@ -3,25 +3,65 @@ import random
 import time
 import argparse
 import socket
+import os
+import sys
+import math
+
+bob2_protocol_path = os.path.abspath("../Bob2/src/protocol")
+sys.path.append(bob2_protocol_path)
+import bob2_protocol
+
+WANT_BOB_MAJOR_VERSION = 0
+WANT_BOB_MINOR_VERSION = 2
 
 class WhaleModel:
-    def __init__(self, num_whales, min_diving_time, max_diving_time, min_surface_time, max_surface_time, satellite_host, satellite_port):
+    def __init__(self, num_whales, min_diving_time, max_diving_time, min_surface_time, max_surface_time, packet_size, satellite_host, satellite_port):
         self.num_whales = num_whales
         self.min_diving_time = min_diving_time
         self.max_diving_time = max_diving_time
         self.min_surface_time = min_surface_time
         self.max_surface_time = max_surface_time
+        self.packet_size = packet_size
         self.satellite_host = satellite_host
         self.satellite_port = satellite_port
+        self.bob2 = bob2_protocol.Bob2Protocol()
 
     # Whale has breached, send data to satellite
-    def send_data(self, whale_id, whale_connection):
-        # TODO make this send data
+    def send_data(self, whale_id, whale_connection, time_since_surfaced):
+        # Random surface time as timeout for the transmission
+        surface_time = random.randint(self.min_surface_time, self.max_surface_time)
+        end_time = time.time() + surface_time
+
+        sample_data = f"whale {whale_id} says: Whale Data wHaLe DaTa whale data WHALE DATA" * time_since_surfaced
+        sample_data = sample_data.encode()
+        packet_count = math.ceil(len(sample_data)/self.packet_size)
+
         whale_connection.connect((self.satellite_host, self.satellite_port))
-        whale_connection.sendall(f"Whale {whale_id} came to the surface at {time.strftime('%X')}".encode())
-        whale_connection.close()
-        # Simulate time spent at the surface - TODO make this cut off data once the time runs out
-        time.sleep(random.randint(self.min_surface_time, self.max_surface_time))
+
+        # Loop through packets - note that Bob2 expects packet count to start at 1
+        for packet_num in range(1, packet_count+1):
+            if packet_num == packet_count+1:
+                content_for_packet = sample_data[(packet_num-1)*self.packet_size:]
+            else:
+                content_for_packet = sample_data[(packet_num-1)*self.packet_size : packet_count*self.packet_size]
+            packet = self.bob2.build_message(
+                message_type=0,
+                dest_ipv6="::1", # Dummy value
+                dest_port=12345, # Dummy value
+                message_content=content_for_packet.decode(), 
+                multiple_packets=True,
+                packet_num=packet_num,
+            )
+            whale_connection.send(packet)
+            # If the surface time has run out, stop sending packets
+            # TODO also let this stop transmission in the middle of a packet!
+            if time.time() >= end_time:
+                whale_connection.close()
+                break
+
+        if time.time() < end_time:
+            time_remaining = end_time-time.time()
+            time.sleep(time_remaining)
 
     # Function to simulate whale diving and resurfacing
     def start_whale_routine(self, whale_id, whale_connection):
@@ -29,7 +69,7 @@ class WhaleModel:
             # Simulate diving delay before resurfacing
             diving_time = random.randint(self.min_diving_time, self.max_diving_time)
             time.sleep(diving_time)
-            self.send_data(whale_id, whale_connection)
+            self.send_data(whale_id, whale_connection, diving_time)
 
     # Method to start all whales' routines
     def start(self):
@@ -41,9 +81,9 @@ class WhaleModel:
         
         print(f"{self.num_whales} whales modeled. Whales are now swimming in the background.")
 
-def main(num_whales, min_diving_time, max_diving_time, min_surface_time, max_surface_time, satellite_host, satellite_port):
+def main(num_whales, min_diving_time, max_diving_time, min_surface_time, max_surface_time, packet_size,  satellite_host, satellite_port):
     # Create an instance of WhaleModel with the provided arguments
-    whale_model = WhaleModel(num_whales, min_diving_time, max_diving_time, min_surface_time, max_surface_time, satellite_host, satellite_port)
+    whale_model = WhaleModel(num_whales, min_diving_time, max_diving_time, min_surface_time, max_surface_time, packet_size, satellite_host, satellite_port)
     # Start the whale model
     whale_model.start()
 
@@ -62,11 +102,15 @@ if __name__ == "__main__":
     parser.add_argument("--max_diving_time", type=int, default=3600, help="The maximum time a whale should spend diving/out of the range of LEO satellites (in seconds, default: 3600)")
     parser.add_argument("--min_surface_time", type=int, default=30, help="The minimum time a whale should spend at the surface (in seconds, default: 30)")
     parser.add_argument("--max_surface_time", type=int, default=1200, help="The maximum time a whale should spend at the surface (in seconds, default: 1200)")
+    parser.add_argument("--packet_size", type=int, default=32, help="Maximum length in bytes which should be included in a message")
     
     # TODO: remove these once code to select satellite is written
     parser.add_argument("--satellite_host", type=str, default="::1", help="Satellite host")
     parser.add_argument("--satellite_port", type=int, default=12345, help="Satellite port")
     args = parser.parse_args()
 
+    if bob2_protocol.CURRENT_MAJOR_VERSION != WANT_BOB_MAJOR_VERSION or bob2_protocol.CURRENT_MINOR_VERSION != WANT_BOB_MINOR_VERSION:
+        print(f"Wrong version of Bob2 installed in ../Bob2/src/protocol ({bob2_protocol.CURRENT_MAJOR_VERSION}.{bob2_protocol.CURRENT_MINOR_VERSION}), want version {WANT_BOB_MAJOR_VERSION}.{WANT_BOB_MINOR_VERSION}")
+
     # Model the specified number of whales
-    main(args.num_whales, args.min_diving_time, args.max_diving_time, args.min_surface_time, args.max_surface_time, args.satellite_host, args.satellite_port)
+    main(args.num_whales, args.min_diving_time, args.max_diving_time, args.min_surface_time, args.max_surface_time, args.packet_size, args.satellite_host, args.satellite_port)
